a96b6a83f981750c678588c471452cc57ebb03de
Sullyvan Moura <sullyvanmoura@hotmail.com>
2022-09-19 16:31:54 -0300

    Colisao e Suavizacao da camera

--- Tomb Invader/Assets/Scripts/Comp_CameraController.cs
+++ Tomb Invader/Assets/Scripts/Comp_CameraController.cs
@@ -12,6 +12,7 @@ namespace CG
         [SerializeField] private Vector3 _framing = new Vector3(0, 0, 0);
 
         [Header("Distance")]
+        [SerializeField] private float _zoomSpeed = 10f;
         [SerializeField] private float _defaultDistance = 5f;
         [SerializeField] private float _minDistance = 0f;
         [SerializeField] private float _maxDistance = 10f;
@@ -19,6 +20,7 @@ namespace CG
         [Header("Rotation")]
         [SerializeField] private bool _invertX = false;
         [SerializeField] private bool _invertY = false;
+        [SerializeField] private float _rotationSharpness = 25f;
         [SerializeField] private float _defaultVerticalAngle = 20f;
         [SerializeField][Range(-90, 90)] private float _minVerticalAngle = -90;
         [SerializeField][Range(-90, 90)] private float _maxVerticalAngle = 90;
@@ -34,6 +36,9 @@ namespace CG
         private Vector3 _targetPosition;
         private Quaternion _targetRotation;
         private float _targetVerticalAngle;
+
+        private Vector3 _newPosition;
+        private Quaternion _newRotation;
 
         private void OnValidate()
         {
@@ -63,7 +68,7 @@ namespace CG
             if (Cursor.lockState != CursorLockMode.Locked) { return; }
 
             //Handle Inputs
-            float _zoom = -Comp_PlayerInputs.MouseScrollInput;
+            float _zoom = -Comp_PlayerInputs.MouseScrollInput*_zoomSpeed;
             float _mouseX = Comp_PlayerInputs.MouseXInput;
             float _mouseY = Comp_PlayerInputs.MouseYInput;
 
@@ -81,14 +86,25 @@ namespace CG
             //Handle Obstructions
             float _smallestDistance = _targetDistance;
             RaycastHit[] _hits = Physics.SphereCastAll(_focusPosition, _checkRadius, _targetRotation * -Vector3.forward, _targetDistance, _obstructionLayers);
+            if (_hits.Length != 0) {
+                foreach (RaycastHit hit in _hits) {
+                    if (!_ignoreColliders.Contains(hit.collider)) 
+                        if (hit.distance < _smallestDistance)
+                            _smallestDistance = hit.distance;
+                }
+            }
 
             //Final Target
             _targetRotation = Quaternion.LookRotation(_planarDirection) * Quaternion.Euler(_targetVerticalAngle, 0, 0);
-            _targetPosition = _followTransform.position - (_targetRotation * Vector3.forward) * _targetDistance;
+            _targetPosition = _focusPosition - (_targetRotation * Vector3.forward) * _smallestDistance;
+
+            //Handle Smoothing
+            _newRotation = Quaternion.Slerp(_camera.transform.rotation, _targetRotation, Time.deltaTime* _rotationSharpness);
+            _newPosition = Vector3.Lerp(_camera.transform.position, _targetPosition, Time.deltaTime * _rotationSharpness);
 
             //Apply
-            _camera.transform.rotation = _targetRotation;
-            _camera.transform.position = _targetPosition;
+            _camera.transform.rotation = _newRotation;
+            _camera.transform.position = _newPosition;
         }
 
     }
