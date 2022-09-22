52ed02bc7393b3e4646852ab29a309a772d62681
Sullyvan Moura <sullyvanmoura@hotmail.com>
2022-09-21 22:52:39 -0300

    Mapa finalizado

--- Tomb Invader/Assets/PauseMenu.cs
+++ Tomb Invader/Assets/PauseMenu.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarterAssets;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-
     public GameObject pauseMenuUI;
     // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -15,14 +16,16 @@ public class PauseMenu : MonoBehaviour
             if (GameIsPaused)
             {
                 Resume();
-            } else {
+            }
+            else
+            {
                 Pause();
             }
         }
 
     }
 
-    void Resume ()
+    void Resume()
     {
         pauseMenuUI.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
@@ -32,10 +35,11 @@ public class PauseMenu : MonoBehaviour
 
     }
 
-    void Pause ()
+    void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+
     }
 }
